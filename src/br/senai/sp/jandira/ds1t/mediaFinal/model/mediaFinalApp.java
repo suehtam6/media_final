f7bb2df2e1242d1cfa4de21f50d4435c17fcc8cd
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2025-09-22 15:02:56 -0300

    Final do trabalho

--- src/br/senai/sp/jandira/ds1t/mediaFinal/model/mediaFinalApp.java
+++ src/br/senai/sp/jandira/ds1t/mediaFinal/model/mediaFinalApp.java
@@ -1,5 +1,15 @@
 package br.senai.sp.jandira.ds1t.mediaFinal.model;
 
+import br.senai.sp.jandira.ds1t.mediaFinal.model.model.mediaFinal;
+
 public class mediaFinalApp {
+
+    public static void main(String[] args) {
+        System.out.println("Aplicativo está sendo iniciado!!");
+
+        mediaFinal mediaFinal = new mediaFinal();
+        mediaFinal.getData();
+
+    }
 
 }
--- src/br/senai/sp/jandira/ds1t/mediaFinal/model/model/mediaFinal.java
+++ src/br/senai/sp/jandira/ds1t/mediaFinal/model/model/mediaFinal.java
@@ -9,62 +9,57 @@ public class mediaFinal {
     double nota2;
     double nota3;
     double nota4;
-    double pesoDaNota1;
-    double pesoDaNota2;
-    double pesoDaNota3;
-    double pesoDaNota4;
+    double media;
+    String situacao;
 
-    public void getData(){
+    public void getData() {
 
         Scanner leitor = new Scanner(System.in);
 
-
+        System.out.println("---------------------------------------");
         System.out.print("Digite o nome do aluno:");
-        nomeAluno = leitor.findInLine();
+        nomeAluno = leitor.nextLine();
 
         System.out.print("Agora digite a nota da atividade 1:");
         nota1 = leitor.nextDouble();
 
 
-        System.out.print("Agora digite o peso da atividade 1:");
-        pesoDaNota1 = leitor.nextDouble();
-
         System.out.print("Agora digite a nota da atividade 2:");
         nota2 = leitor.nextDouble();
-
-
-        System.out.print("Agora digite o peso da atividade 2:");
-        pesoDaNota2 = leitor.nextDouble();
 
 
         System.out.print("Agora digite a nota da atividade 3:");
         nota3 = leitor.nextDouble();
 
-
-        System.out.print("Agora digite o peso da atividade 3:");
-        nota3 = leitor.nextDouble();
-
-
-        System.out.print("Agora digite o peso da atividade 3:");
-        pesoDaNota3 = leitor.nextDouble();
-
         System.out.print("Agora digite a nota da atividade 4:");
         nota4 = leitor.nextDouble();
-
-
-        System.out.print("Agora digite o peso da atividade 4:");
-        pesoDaNota4 = leitor.nextDouble();
+        System.out.println("---------------------------------------");
 
         calculandoNotas();
 
     }
 
-    public void calculandoNotas(){
+    public void calculandoNotas() {
 
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
 
+        if (media >= 5) {
+            situacao = "Aprovado(a)";
+        } else {
+            situacao = "Reprovado(a)";
+        }
 
-
+        exibirResultados();
     }
 
+
+    public void exibirResultados() {
+
+        System.out.println("************************************************");
+        System.out.println("Média do aluno: " + media);
+        System.out.println("Resultado: O(a) " + nomeAluno + " está " + situacao);
+        System.out.println("************************************************");
+
+    }
 
 }
